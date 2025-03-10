a62f76d8c800086fd280819b68a89fd7b59f22ea
enzk0 <renzoroel24@gmail.com>
2025-03-10 19:02:39 +0800

    Yippe

--- Backend/Qr Code Generator/index.js
+++ Backend/Qr Code Generator/index.js
@@ -1,16 +1,33 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "node:fs";
 
 inquirer
     .prompt([
-    /* Pass your questions in here */
+        {
+          name: "url",
+          type: "input",
+          message: "Please place a url to convert in QR code!"
+      },
     ])
     .then((answers) => {
-    // Use user feedback for... whatever!!
+      const url = answers.url;
+      makeQr(url);
     })
     .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      console.error("An error occurred:", error);
     } else {
       // Something else went wrong
     }
 });
+
+function makeQr(key){
+  var qr_image = qr.image(key)
+  qr_image.pipe(fs.createWriteStream("qr_code.png"))
+
+  fs.writeFile("URL.txt", qr_img, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+}
